8da1671761f58828c528c842505957bb732663b0
Lissa Deguti <lilideguti@gmail.com>
2025-07-30 17:06:23 -0300

    a lot
    just so i dont lose anything while im trying to put a customized font

---
+++ Untitled.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  borrowers
+//
+//  Created by Lissa Deguti on 30/07/25.
+//
+
---
+++ borrowers/Views/Shared/TextTypingView.swift
@@ -0,0 +1,70 @@
+//
+//  TextTypingView.swift
+//  borrowers
+//
+//  Created by Lissa Deguti on 30/07/25.
+//
+
+import SwiftUI
+
+struct TextTypingView: View {
+    let fullText: String
+    @Binding var isTyping: Bool
+    @Binding var textFullyDisplayed: Bool
+    var textColor: Color = .white
+    
+    @State private var displayedText = ""
+    @State private var timer: Timer?
+    
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            Text(displayedText)
+                .font(.system(size: 20, weight: .regular, design: .default))
+                .multilineTextAlignment(.leading)
+                .foregroundColor(textColor)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .onAppear {
+                    startTyping()
+                }
+                .onChange(of: isTyping) {
+                    if !isTyping && displayedText != fullText {
+                        showFullText()
+                    }
+                }
+            
+            if textFullyDisplayed {
+                Image(systemName: "chevron.right")
+                    .foregroundColor(textColor)
+                    .padding()
+                    .transition(.opacity)
+            }
+        }
+    }
+    
+    private func startTyping() {
+        displayedText = ""
+        textFullyDisplayed = false
+        var charIndex = 0
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.035, repeats: true) { timer in
+            if charIndex < fullText.count {
+                let index = fullText.index(fullText.startIndex, offsetBy: charIndex)
+                displayedText += String(fullText[index])
+                charIndex += 1
+            } else {
+                timer.invalidate()
+                isTyping = false
+                textFullyDisplayed = true
+            }
+        }
+    }
+    
+    private func showFullText() {
+        timer?.invalidate()
+        displayedText = fullText
+        isTyping = false
+        textFullyDisplayed = true
+    }
+}
+
